3c43eb8c13f90177d9e92284f1ae1436ce231905
Joachim MASSON <joachim.masson.17@gmail.com>
2025-10-14 17:39:04 +0200

    Add filter Artist on html, css and js

--- musique/musiqueScript.js
+++ musique/musiqueScript.js
@@ -1,3 +1,5 @@
+
+// ---------Carousel -----------
 document.querySelectorAll('.carousel').forEach(carousel => {
 
     const track = carousel.querySelector('.carousel-track');
@@ -31,3 +33,71 @@ document.querySelectorAll('.carousel').forEach(carousel => {
         updateSlidePosition();
     });
 });
+
+// ---------------Recherche par Artist---------------
+const resultatRechercheMusique = document.querySelector(".resultatRechercheMusique");
+
+const btnSearchArtist = document.getElementById("btnSearchArtist")
+btnSearchArtist.addEventListener("click", () => {
+   const artistSearch = document.getElementById("artistSearch").value.toLowerCase();
+	
+
+	// Vide les résultats précédents
+  resultatRechercheMusique.innerHTML = "";
+
+  // Chargement du fichier JSON
+  fetch("musique.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Erreur de chargement du fichier JSON");
+		}
+		return response.json();
+    })
+    .then(data => {
+      // Filtrage des artistes correspondant à la recherche
+      const filteredArtists = data.filter(artist =>
+        artist.artist.toLowerCase().includes(artistSearch)
+      );
+		//Affichage du résultat
+      if (filteredArtists.length > 0) {
+        filteredArtists.forEach(artist => {
+			const resultatImagePochette = document.createElement("img");
+			resultatImagePochette.src=artist.image;
+			resultatRechercheMusique.appendChild(resultatImagePochette);
+
+			const resultatContenu = document.createElement("div");
+			resultatContenu.classList.add("description");
+			const resultatTitre = document.createElement("h3");
+			resultatTitre.textContent = `${artist.artist} - ${artist.album}`;
+			resultatContenu.appendChild(resultatTitre);
+			const resultatDescription = document.createElement("p");
+			resultatDescription.textContent = `${artist.description}`;
+			resultatContenu.appendChild(resultatDescription);
+			resultatRechercheMusique.appendChild(resultatContenu);
+
+			const resultatAudio = document.createElement("div");
+			resultatAudio.classList.add("audio");
+			const resultatTitleAudio = document.createElement("h5");
+			resultatTitleAudio.textContent = "Ecouter un extrait";
+			resultatAudio.appendChild(resultatTitleAudio);
+			const extraitAudio = document.createElement("audio");
+			extraitAudio.src=artist.music;
+			extraitAudio.controls = true;
+			resultatAudio.appendChild(extraitAudio);
+			resultatRechercheMusique.appendChild(resultatAudio);
+        });
+      } else {
+        resultatRechercheMusique.textContent = "Aucun artiste trouvé.";
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      resultatRechercheMusique.textContent = "Une erreur est survenue lors du chargement des données.";
+    });
+});
+
+// Recherche par Album
+const btnSearchAlbum = document.getElementById("btnSearchAlbum")
+btnSearchAlbum.addEventListener("click", () => {
+   const albumSearch = document.getElementById("albumSearch").value.toLowerCase();
+
